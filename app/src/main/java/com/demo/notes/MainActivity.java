ad4f6a2d86b11e3e4de57678dc46b9ad0a324eb1
alex <alex@localhost.localdomain>
2020-03-31 02:20:14 +0300

    first commit

---
+++ app/src/main/java/com/demo/notes/MainActivity.java
@@ -0,0 +1,44 @@
+package com.demo.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerViewNotes;
+    public static final ArrayList<Note> notes = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (notes.isEmpty()) {
+            notes.add(new Note("Парикмахер", "Сделать прическу", "Понедельник", 2));
+            notes.add(new Note("Баскетбол", "Игра со школьной командой", "Вторник", 3));
+            notes.add(new Note("Магазин", "Купить новые джинсы", "Понедельник", 3));
+            notes.add(new Note("Стоматолог", "Вылечить зубы", "Понедельник", 2));
+            notes.add(new Note("Парикмахер", "Сделать прическу к выпускному", "Среда", 1));
+            notes.add(new Note("Баскетбол", "Игра со школьной командой", "Вторник", 3));
+            notes.add(new Note("Магазин", "Купить новые джинсы", "Понедельник", 3));
+        }
+
+            NotesAdapter adapter = new NotesAdapter(notes);
+
+            recyclerViewNotes = findViewById(R.id.recyclerViewNotes);
+            recyclerViewNotes.setLayoutManager(new LinearLayoutManager(this));
+            recyclerViewNotes.setAdapter(adapter);
+    }
+
+    public void onClickAddNote(View view) {
+        Intent intent = new Intent(this, AddNoteActivity.class);
+        startActivity(intent);
+    }
+}
